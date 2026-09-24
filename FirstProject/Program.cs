fdffd76b84a12b628b54668a3364928cc3926dc0
AmolBorude-cpu <borude.amol@unitedglobalfederation.org>
2026-09-24 17:56:20 +0530

    Add C# first console project

---
+++ FirstProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 22;
+        string name = "Amol";
+        double percentage = 85.5;
+        char grade = 'A';
+        bool isStudent = true;
+
+        const int DAYS_IN_WEEK = 7;
+
+        Console.WriteLine(name);
+        Console.WriteLine(age);
+        Console.WriteLine(percentage);
+        Console.WriteLine(grade);
+        Console.WriteLine(isStudent);
+        Console.WriteLine(DAYS_IN_WEEK);
+    }
+}
