ebe393845b81532ea715c97758eb7e1ec048e419
AmolBorude-cpu <borude.amol@unitedglobalfederation.org>
2026-09-25 14:55:13 +0530

    Add type casting program

---
+++ TypecastingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Implicit casting
+        int number = 100;
+        double value = number;
+
+        Console.WriteLine("Integer value: " + number);
+        Console.WriteLine("Double value: " + value);
+
+        // Explicit casting
+        double price = 99.99;
+        int newPrice = (int)price;
+
+        Console.WriteLine("Original double value: " + price);
+        Console.WriteLine("After casting to int: " + newPrice);
+    }
+}
